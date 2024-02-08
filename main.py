8bb88fe35782f958498fd1ed35231d08b71fc84f
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2024-02-08 01:59:32 -0500

    thats a very useful tool

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import itertools
 
 def f(args):
     a, b = args
-    if (0.1215 < a/b < 0.1216):
+    if (0.1494 < a/b < 0.15):
         return (a, b)
 
 
-l1 = [14.34, 15.34]
-l2 = [115.00 + i * 0.01 for i in range(int((130.00-115.00) / 0.01))]
+l1 = [53]
+l2 = [320.00 + i * 0.01 for i in range(int((400.00-320.00) / 0.01))]
 
 if __name__ == "__main__":
     with Pool(7) as p:
